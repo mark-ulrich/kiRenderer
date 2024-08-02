5a0811a0f5ff759acc98eab795a19a50c5eb0729
Mark Ulrich <mark.ulrich.86@gmail.com>
2024-08-01 23:55:02 -0500

    Add static unpack methods to kiColor

--- include/kiColor.h
+++ include/kiColor.h
@@ -1,7 +1,5 @@
-#ifndef  __KICOLOR_H__
-#define  __KICOLOR_H__
-
-#include "kiTypes.h"
+#ifndef __KICOLOR_H__
+#define __KICOLOR_H__
 
 #include "macros.h"
 #include "types.h"
@@ -10,13 +8,27 @@ struct kiColor
 {
   float r, g, b, a;
 
-  kiColor() : r(0.0f), g(0.0f), b(0.0f), a(0.0f) {}
+  kiColor()
+    : r(0.0f)
+    , g(0.0f)
+    , b(0.0f)
+    , a(0.0f)
+  {
+  }
 
-  kiColor(float r, float g, float b, float a = 0.0f) :
-    r(r), g(g), b(b), a(a) {}
+  kiColor(float r, float g, float b, float a = 0.0f)
+    : r(r)
+    , g(g)
+    , b(b)
+    , a(a)
+  {
+  }
 
-  u32 PackARGB() const;
+  FE_NODISCARD u32 PackARGB() const;
+
+  static kiColor FromARGB(u32 packed);
+  static kiColor FromRGBA(u32 packed);
+  static kiColor FromRGB(u32 packed);
 };
-
 
 #endif //  __KICOLOR_H__
--- src/kiColor.cpp
+++ src/kiColor.cpp
@@ -10,3 +10,43 @@ kiColor::PackARGB() const
   packed |= (int)(b * 0xff);
   return packed;
 }
+
+kiColor
+kiColor::FromARGB(u32 packed)
+{
+  u8 a = (packed >> 24) & 0xff;
+  u8 r = (packed >> 16) & 0xff;
+  u8 g = (packed >> 8) & 0xff;
+  u8 b = packed & 0xff;
+  kiColor color(static_cast<float>(r) / 255.0f,
+                static_cast<float>(g) / 255.0f,
+                static_cast<float>(b) / 255.0f,
+                static_cast<float>(a) / 255.0f);
+  return color;
+}
+
+kiColor
+kiColor::FromRGBA(u32 packed)
+{
+  u8 r = (packed >> 24) & 0xff;
+  u8 g = (packed >> 16) & 0xff;
+  u8 b = (packed >> 8) & 0xff;
+  u8 a = packed & 0xff;
+  kiColor color(static_cast<float>(r) / 255.0f,
+                static_cast<float>(g) / 255.0f,
+                static_cast<float>(b) / 255.0f,
+                static_cast<float>(a) / 255.0f);
+  return color;
+}
+
+kiColor
+kiColor::FromRGB(u32 packed)
+{
+  u8 r = (packed >> 16) & 0xff;
+  u8 g = (packed >> 8) & 0xff;
+  u8 b = packed & 0xff;
+  kiColor color(static_cast<float>(r) / 255.0f,
+                static_cast<float>(g) / 255.0f,
+                static_cast<float>(b) / 255.0f);
+  return color;
+}
